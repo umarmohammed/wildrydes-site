aa2c16602f7bc86b0c44565070561992a0e4a5cb
Umar Mohammed <umar.mohammed@holisticai.com>
2021-09-21 14:02:08 +0100

    add api invokUrl

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'eu-west-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://nui2xd4214.execute-api.eu-west-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
